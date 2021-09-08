3231cce147c517df43eb3662e5dcbd8701951ba5
Ioannis <iostavri@microsoft.com>
2021-09-08 11:25:40 +0300

    Added kubctl secret config

---
+++ Challenges/Challenge3/create-secrets-config.sh
@@ -0,0 +1,3 @@
+kubectl create secret generic sql --from-literal=SQL_USER=sqladminsLg1438 \ 
+--from-literal=SQL_PASSWORD=yS8vj8Ai0 --from-literal=SQL_SERVER=sqlserverslg1438.database.windows.net \
+--from-literal=SQL_DBNAME=mydrivingDB
